2a8cf6d399ccb585d692d4ac9a91c277932d32bc
Akzhol <akzhol.nuradinov@gmail.com>
2026-01-03 10:04:11 +0000

    loaded model

---
+++ test_artifac_model.sh
@@ -0,0 +1,9 @@
+export MLFLOW_S3_ENDPOINT_URL=https://storage.yandexcloud.net
+export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=$S3_SECRET_KEY
+
+mlflow server \
+    --backend-store-uri postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME \
+	--registry-store-uri postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME \
+    --default-artifact-root s3://$AWS_BUCKET_NAME \
+	--no-serve-artifacts
